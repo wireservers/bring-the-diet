27192316a70c869c8f19c285518ee2c13acfc22d
Todd-WS <todd@wireservers.com>
2026-04-01 20:17:57 -0500

    fix: outputFileTracingRoot should be ../ not ../../

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -6,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../../'),
+  outputFileTracingRoot: path.join(__dirname, '../'),
   reactStrictMode: true,
   transpilePackages: ['@bringthediet/ui', '@bringthediet/shared'],
 };
